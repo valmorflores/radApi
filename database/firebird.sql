8df38057e943c788517907877eda91d9a2b4f0a5
valmorflores <valmorflores@gmail.com>
2022-06-04 18:20:48 -0300

    update sql command

--- database/firebird.sql
+++ database/firebird.sql
@@ -27,48 +27,44 @@ CREATE TABLE TBLUSERTOKEN (
     EXPIRES_AT_TIME                 TIME
 );
 
----MYSQL--
-CREATE TABLE `TBLSTAFF` (
-  `STAFFID` int(11) NOT NULL,
-  `EMAIL` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `FIRSTNAME` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `LASTNAME` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `FACEBOOK` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `LINKEDIN` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `PHONENUMBER` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `SKYPE` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `PASSWORD` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `DATECREATED` date DEFAULT NULL,
-  `PROFILE_IMAGE` varchar(300) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `LAST_IP` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `LAST_LOGIN` date DEFAULT NULL,
-  `LAST_ACTIVITY` date DEFAULT NULL,
-  `LAST_PASSWORD_CHANGE` date DEFAULT NULL,
-  `NEW_PASS_KEY` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `NEW_PASS_KEY_REQUESTED` date DEFAULT NULL,
-  `ADMIN` int(11) DEFAULT NULL,
-  `ROLE` int(11) DEFAULT NULL,
-  `ACTIVE` int(11) DEFAULT NULL,
-  `DEFAULT_LANGUAGE` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `DIRECTION` varchar(3) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `MEDIA_PATH_SLUG` varchar(300) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `IS_NOT_STAFF` int(11) DEFAULT NULL,
-  `HOURLY_RATE` decimal(15,2) DEFAULT NULL,
-  `TWO_FACTOR_AUTH_ENABLED` varchar(1) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `TWO_FACTOR_AUTH_CODE` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `TWO_FACTOR_AUTH_CODE_REQUESTED` date DEFAULT NULL,
-  `EMAIL_SIGNATURE` text COLLATE utf8_unicode_ci,
-  `LAST_LOGIN_TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `LAST_ACTIVITY_TIME` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `LAST_ACTIVE_TIME` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE TBLSTAFF (
+    STAFFID                         INT(11) NOT NULL,
+    EMAIL                           VARCHAR(100),
+    FIRSTNAME                       VARCHAR(50),
+    LASTNAME                        VARCHAR(50),
+    FACEBOOK                        VARCHAR(255),
+    LINKEDIN                        VARCHAR(255),
+    PHONENUMBER                     VARCHAR(30),
+    SKYPE                           VARCHAR(50),
+    PASSWORD                        VARCHAR(250),
+    DATECREATED                     DATE,
+    PROFILE_IMAGE                   VARCHAR(300),
+    LAST_IP                         VARCHAR(40),
+    LAST_LOGIN                      DATE,
+    LAST_ACTIVITY                   DATE,
+    LAST_PASSWORD_CHANGE            DATE,
+    NEW_PASS_KEY                    VARCHAR(32),
+    NEW_PASS_KEY_REQUESTED          DATE,
+    ADMIN                           INT(11),
+    ROLE                            INT(11),
+    ACTIVE                          INT(11),
+    DEFAULT_LANGUAGE                VARCHAR(40),
+    DIRECTION                       VARCHAR(3),
+    MEDIA_PATH_SLUG                 VARCHAR(300),
+    IS_NOT_STAFF                    INT(11),
+    HOURLY_RATE                     DECIMAL(15,2),
+    TWO_FACTOR_AUTH_ENABLED         VARCHAR(1),
+    TWO_FACTOR_AUTH_CODE            VARCHAR(100),
+    TWO_FACTOR_AUTH_CODE_REQUESTED  DATE,
+    EMAIL_SIGNATURE                 BLOB segment 4096, subtype TEXT,
+    LAST_LOGIN_TIME                 TIMESTAMP ,
+    LAST_ACTIVITY_TIME              TIMESTAMP,
+    LAST_ACTIVE_TIME                TIMESTAMP
+    ); 
 
 --
--- Índices de tabelas apagadas
+-- Índices de tabela TBLSTAFF
 --
-
---
--- Índices de tabela `TBLSTAFF`
---
-ALTER TABLE `TBLSTAFF`
-  ADD PRIMARY KEY (`STAFFID`);
+ALTER TABLE TBLSTAFF
+  ADD PRIMARY KEY (STAFFID);
