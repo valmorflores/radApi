5f3387f143a5f575edd2826fc9ce27992d1096e2
marleirafa <91739657+marleirafa@users.noreply.github.com>
2022-06-15 13:41:28 -0300

    Count from table

--- app/Controllers/TablesData.php
+++ app/Controllers/TablesData.php
@@ -57,7 +57,41 @@ class TablesData extends ResourceController
             return $this->respond($response);
         }
     }
+    public function getdatacount($table = null, $offset, $records) {
 
+        $info = new BaseController();
+        $loadResult = $info->loadAuthorization($this->request);
+        if (isset($loadResult['error_status'])){
+            // Error from load (unknow authorization data)
+            return $this->respond($loadResult);
+        }
+        else
+        {
+            $dataSet = $this->TableDataModel->getFromTable($table, $offset, $records);
+            $list=[];
+
+            foreach ($dataSet as $row)
+            {
+                $info = $row;
+                $list[]=$info;
+            }
+            $data = [];
+            $data = $list;
+            $resp = $data;
+            $response = [
+                'parameter_table_name' => $table,
+                'parameter_offset' => $offset,
+                'parameter_records' => $records,
+                'status'   => 200,
+                'error'    => null,
+                'data'     => count($resp),
+                'messages' => [
+                    'success' => 'There are ' . count($resp) . ' records in this table'
+                    ]
+                ];
+            return $this->respond($response);
+        }
+    }
     // getDataBy like: $path/v1/tables/data-by/TBLOPTIONS/ID/84
     public function getdataby($table = null, $field, $information) {
         $info = new BaseController();
